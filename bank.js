3d6ef1c13fce52b0fee9ca5127539ca3af2e5afb
Christian Pinna <christian.a.pinna@gmail.com>
2022-04-25 16:14:09 +0100

    bank class finished, will follow a red, greeen commit, refactor, commit on my next project

---
+++ bank.js
@@ -0,0 +1,30 @@
+class Bank {
+  constructor() {
+    this.balance = 0;
+    this.transactions = [];
+  }
+  accountStatement() {
+    if (this.#noTransactions) {
+      return "date || credit || debit || balance\n || || || ";
+    } else {
+      this.transactions.reverse();
+      this.transactions.unshift("date || credit || debit || balance");
+      return this.transactions.join('');
+    }
+    
+  }
+  deposite(ammount, date) {
+    this.balance += ammount;
+    this.transactions.push(`\n${date} || ${ammount}.00 || || ${this.balance}.00`);
+  }
+  withdrawal(ammount, date) {
+    this.balance -= ammount;
+    this.transactions.push(`\n${date} || || ${ammount}.00 || ${this.balance}.00`);
+  }
+
+  #noTransactions() {
+    return this.transactions.length === 0;
+  }
+}
+
+module.exports = Bank;
